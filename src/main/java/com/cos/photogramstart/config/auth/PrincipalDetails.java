b682f25c8fa59f8aa8a0bf8cc990e69178dba3ab
leesuonghoon <fecd119@naver.com>
2023-11-23 03:36:31 +0900

    Ajex

---
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
@@ -0,0 +1,77 @@
+package com.cos.photogramstart.config.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.Data;
+
+
+@Data
+public class PrincipalDetails implements UserDetails{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private User user;
+	
+	public PrincipalDetails(User user) {
+		this.user = user;
+	}
+
+	//권한이라는게 한개가 아닐 수 있음.(3개 이상의 권한)
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		//권한을 가져오는 함수
+		
+		Collection<GrantedAuthority> collector = new ArrayList<>();
+		collector.add(() -> {
+				return user.getRole();
+		});
+		
+		return collector;
+		
+	}
+
+	@Override
+	public String getPassword() {
+		
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		//계정만료 ?
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		//락?
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		//비밀번호가 만료 ?
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		//로그인 안한 기간 ?
+		return true;
+	}
+
+}
--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,4 +1,22 @@
 // (1) 회원정보 수정
-function update() {
-
+function update(userid) {
+	
+	let date = $("#profileUpdate").serialize();
+	
+	console.log(date);
+	
+	$.ajax({
+		type:"put",
+		url:`/api/user/${userid}`,
+		data : data,
+		contentType:"application/x-www-form-urlencoded; charset=urf-8",
+		dataType:"json"
+	}).done(res=>{
+		console.log("update 성공");
+		location.herf=`user/${userid}`;
+	}).fail(error=>{
+		console.log("update 실패");
+	})
+	
+	
 }
