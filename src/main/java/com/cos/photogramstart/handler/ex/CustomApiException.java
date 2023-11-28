99ecaebd2631e403da1e4d03dcdedf6295915364
leesuonghoon <fecd119@naver.com>
2023-11-29 01:10:24 +0900

    upload

--- src/main/java/com/cos/photogramstart/handler/ex/CustomApiException.java
+++ src/main/java/com/cos/photogramstart/handler/ex/CustomApiException.java
@@ -10,23 +10,11 @@ public class CustomApiException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	// 객체를 구분할 때 쓸 씨리얼번호
 	
-	/* private String message; */
-	private Map<String,String> errorMap;
-	
 	public CustomApiException(String message) {
 		super(message);
 	}
+
 	
-	public CustomApiException(String message, Map<String, String> errorMap) {
-		super(message);
-		//생성자
-		/* this.message = message; */
-		this.errorMap = errorMap;
-	}
-	
-	public Map<String,String> getErrorMap() {
-		return errorMap;
-	}
 	
 
 }
